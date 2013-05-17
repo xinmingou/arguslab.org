b85f9074444a1863925b65626cbfc690d34543b0
Cole Cooper <cole@chrono101.com>
2013-05-17 15:03:44 -0500

    Changing site structure to accomdate the ability to have one menu loaded for all pages with jQuery

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function() {
+  $.get("menu.html", function(data) { 
+    $("#navigation-menu").append(data); 
+    var page = window.location.href.slice(window.location.href.lastIndexOf("/")+1);
+    $("a[href*='" + page + "']").parent().addClass("active");
+  });
+});
