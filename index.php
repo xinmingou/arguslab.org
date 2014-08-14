004be00a9a4da77f72b90b89f6833e25bbac1bbd
gilneidp <gilnei@gilnei.com.br>
2014-08-14 14:31:44 -0500

    Update index.php
    Taken out bootstrap responsive

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
   <title>Argus Group</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="css/bootstrap-responsive.css" rel="stylesheet">
   
   <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
   
